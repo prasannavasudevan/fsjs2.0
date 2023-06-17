69b5f09fa41f19e46e371a4502b3adc5fad24b14
prasanna <32860910+prasannavasudevan@users.noreply.github.com>
2023-06-17 11:57:02 +0530

    Add files via upload

---
+++ Projects (javascript)/09 question/script.js
@@ -0,0 +1,37 @@
+let name = "";
+let num = 0;
+let age;
+let second = 1;
+let first = "1";
+
+if(name == true){
+    console.log("the value is true")
+}
+else{
+    console.log("The value is false");
+}
+if(num == true){
+    console.log("the value is true")
+}
+else{
+    console.log("The value is false");
+}
+
+if(age == true){
+    console.log("the value is true")
+}
+else{
+    console.log("The value is false");
+}
+if(first == true){
+    console.log("the value is true")
+}
+else{
+    console.log("The value is false");
+}
+if(second == true){
+    console.log("the value is true")
+}
+else{
+    console.log("The value is false");
+}
