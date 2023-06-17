d837095c3402580809bd033c26f3c1a22479dbcf
prasanna <32860910+prasannavasudevan@users.noreply.github.com>
2023-06-17 11:58:32 +0530

    Add files via upload

---
+++ Projects (javascript)/10 question/script.js
@@ -0,0 +1,24 @@
+// Results for the given comparison expressions
+// 1. 4 > 3 is true
+// 2. 4 >= 3 is true
+// 3. 4 < 3 is false
+// 4. 4 <= 3 is false
+// 5. 4 == 4 is true
+// 6. 4 === 4 is true
+// 7. 4 != 4 is false
+// 8. 4 !== 4 is false
+// 9. 4 != '4' is false
+// 10. 4 == '4' is true
+// 11. 4 === '4' is false
+
+console.log("1.", 4>3);
+console.log("2.", 4>=3);
+console.log("3.", 4<3);
+console.log("4.", 4<=3);
+console.log("5.", 4==4);
+console.log("6.", 4===4);
+console.log("7.", 4!=4);
+console.log("8.", 4!==4);
+console.log("9.", 4!='4');
+console.log("10.", 4=='4');
+console.log("11.", 4==='4');
