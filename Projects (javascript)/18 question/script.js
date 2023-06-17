f7d7bb603f762bab85d8bf89b1f0c1564723b585
prasanna <32860910+prasannavasudevan@users.noreply.github.com>
2023-06-17 12:09:05 +0530

    Add files via upload

---
+++ Projects (javascript)/18 question/script.js
@@ -0,0 +1,33 @@
+const prompt = require("prompt-sync")({ sigint: true });
+
+let month = prompt("Enter the month: ");
+let year = parseInt(prompt("Enter the year: "));
+
+// Define an object with the number of days in each month
+const daysInMonth = {
+    january: 31,
+    february: 28,
+    march: 31,
+    april: 30,
+    may: 31,
+    june: 30,
+    july: 31,
+    august: 31,
+    september: 30,
+    october: 31,
+    november: 30,
+    december: 31,
+}
+
+// Check if it's a leap year
+if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    daysInMonth.february = 29;
+}
+
+// Get the number of days based on the input month
+if (month in daysInMonth) {
+    let numDays = daysInMonth[month];
+    console.log(`The number of days in ${month} ${year} is ${numDays}.`);
+} else {
+    console.log("Enter a valid month name.");
+}
