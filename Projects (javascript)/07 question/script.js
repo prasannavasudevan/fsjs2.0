8473d1ec1021f62b056e0a4f8b8add7aa15b0b39
prasanna <32860910+prasannavasudevan@users.noreply.github.com>
2023-06-17 11:54:24 +0530

    Add files via upload

---
+++ Projects (javascript)/07 question/script.js
@@ -0,0 +1,5 @@
+let arr = ["hitesh sir", "anurag sir", "pw skills", "aniruth sir", "team and crew", "pw skills"];
+let first = arr.indexOf("pw skills");
+console.log(`The first occurence of pw skills is at the index: ${first}`);
+let last = arr.lastIndexOf("pw skills");
+console.log(`The Last occurence of pw skills is at the index: ${last}`);
